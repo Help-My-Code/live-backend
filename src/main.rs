394b46e5120216a2abfd3a447974610e30c6804d
Swann HERRERA <swann@devloup.dev>
2022-05-05 10:58:19 +0200

    test with warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{Filter};
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello")
+        .and(warp::path::param())
+        .and(warp::get())
+        .and(warp::header("user-agent"))
+        .and(handle_rest);
+    let hi = warp::path("hi").map(|| "Hello, World!");
+    let ws = warp::path!("ws")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            println!("New connection");
+        });
+    let routes = hello.or(hi);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn handle_rest(param: String, agent: String) -> String {
+    format!("Hello {}, whose agent is {}", param, agent)
 }
