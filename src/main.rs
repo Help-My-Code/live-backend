68978a05e7874a4870830d7b13d8192e8502c06b
Swann HERRERA <swann@devloup.dev>
2022-05-07 13:31:59 +0200

    refacto

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use axum::{
     Router,
 };
 use std::net::SocketAddr;
+use axum::routing::MethodRouter;
 use tower_http::{
     services::ServeDir,
     trace::{DefaultMakeSpan, TraceLayer},
@@ -17,6 +18,36 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    enable_logger();
+
+    let layer = TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::default().include_headers(true));
+    let app = Router::new()
+        .fallback(fallback_route())
+        .route("/ws", get(ws_handler))
+        .layer(layer);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+fn fallback_route() -> MethodRouter {
+    get_service(
+        ServeDir::new("assets").append_index_html_on_directories(true),
+    )
+        .handle_error(|error: std::io::Error| async move {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Unhandled internal error: {}", error),
+            )
+        })
+}
+
+fn enable_logger() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG")
@@ -24,36 +55,6 @@ async fn main() {
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
-
-    // build our application with some routes
-    let app = Router::new()
-        .fallback(
-            get_service(
-                ServeDir::new("assets").append_index_html_on_directories(true),
-            )
-                .handle_error(|error: std::io::Error| async move {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Unhandled internal error: {}", error),
-                    )
-                }),
-        )
-        // routes are matched from bottom to top, so we have to put `nest` at the
-        // top since it matches all routes
-        .route("/ws", get(ws_handler))
-        // logging so we can see whats going on
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-        );
-
-    // run it with hyper
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
 }
 
 async fn ws_handler(
