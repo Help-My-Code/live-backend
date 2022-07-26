a48a92d49a4b34ff9c1a0ee72a81c115c435f6df
Swann HERRERA <swann@devloup.dev>
2022-07-27 00:37:20 +0200

    change env

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const REDIS_URL: &'static str = "redis://127.0.0.1/";
+pub const REDIS_URL: &'static str = "redis://:mypassword@45.147.98.195:30666";
 pub const PORT: u16 = 8080;
 pub const EXPOSED_IP: &'static str = "0.0.0.0";
